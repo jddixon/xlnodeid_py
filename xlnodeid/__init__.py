f4662aac5497fe888c51bdb2741d10a458000db8
Jim Dixon <jddixon@gmail.com>
2017-03-04 13:12:29 -0800

    add pylint, coverage, test_basics.py

--- xlnodeid/__init__.py
+++ xlnodeid/__init__.py
@@ -2,8 +2,8 @@
 
 """ NodeID library for python XLattice packages. """
 
-__version__ = '0.0.2'
-__version_date__ = '2017-03-01'
+__version__ = '0.0.3'
+__version_date__ = '2017-03-04'
 
 __all__ = ['__version__', '__version_date__', 'XLNodeIDError', ]
 
